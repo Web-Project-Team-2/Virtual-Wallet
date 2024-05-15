082b011c1f28d44bb9c847e541f7089e6ca95485
eahristovaa5 <eahristovaa@gmail.com>
2024-05-15 18:02:20 +0300

    responses

---
+++ common/responses.py
@@ -0,0 +1,26 @@
+from fastapi import Response
+
+
+class BadRequest(Response):
+    def __init__(self, content=''):
+        super().__init__(status_code=400, content=content)
+
+
+class NotFound(Response):
+    def __init__(self, content=''):
+        super().__init__(status_code=404, content=content)
+
+
+class Unauthorized(Response):
+    def __init__(self, content=''):
+        super().__init__(status_code=401, content=content)
+
+
+class NoContent(Response):
+    def __init__(self):
+        super().__init__(status_code=204)
+
+
+class InternalServerError(Response):
+    def __init__(self):
+        super().__init__(status_code=500)
