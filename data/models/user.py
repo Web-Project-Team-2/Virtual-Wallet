cd6e970f9da6683ff21c82841f7444cdb2b3f1dd
Dani_Dimitrov <mahckadani@gmail.com>
2024-05-15 18:05:32 +0300

    user model

--- data/models/user.py
+++ data/models/user.py
@@ -1,20 +1,36 @@
-from pydantic import BaseModel, constr, conint, Field
+from pydantic import BaseModel, constr
 from typing import Optional
 from datetime import datetime
+import re
+
+pattern = r'^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}|:"<>?[\];\',.\/\\]).{8,}$'
+
+class Role:
+    ADMIN = 'admin'
+    STANDARD = 'standard'
 
 class User(BaseModel):
-    id: int | None
-    username: constr()
-    password: str
+    id: int | None = None
+    username: constr(min_length=2, max_length=20)
+    password: constr(pattern=pattern)
     email: str
-    phone_number: str
+    phone_number: constr(strip_whitespace=True, min_length=10, max_length=10)
+    role: str = Role.STANDARD
 
 
     @classmethod
-    def from_query_result(cls, id, username, password, email):
+    def from_query_result(cls, id, username, password, email, phone_number, role):
         return cls(
             id=id,
             username=username,
             password=password,
-            email=email)
+            email=email,
+            phone_number=phone_number,
+            role=role
+        )
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+    email: str | None = None
 
