8582a85854b14c8e9b9236843e5822d703b16a18
eahristovaa5 <eahristovaa@gmail.com>
2024-05-18 15:14:52 +0530

    transactions_router_added

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from routers.categories import categories_router
 from routers.users import users_router
+from routers.transactions import transactions_router
 import uvicorn
 
 
 app = FastAPI()
 app.include_router(categories_router)
 app.include_router(users_router)
+app.include_router(transactions_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
