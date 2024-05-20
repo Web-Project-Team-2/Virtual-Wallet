90e739a064f487b4f469bff6ba3957f9f1607c64
eahristovaa5 <eahristovaa@gmail.com>
2024-05-20 15:49:46 +0530

    transactions

--- routers/transactions.py
+++ routers/transactions.py
@@ -1,44 +1,55 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
 from common.responses import NotFound, BadRequest, InternalServerError, Unauthorized, NoContent
+from common.authorization import get_current_user
 from data.models.transactions import Transaction
 from schemas import transactions
 from services import transactions_service
 from datetime import datetime, timedelta
 
-# Add money to wallet - to transfer money between own credit/debit, MUST - in progress
-# Make transaction  - to transfer money to other users, MUST - in progress
-# Approve/decline transaction  - to confirm, deny transactions. MUST - need help
-# List transactions - view their transactions. MUST - in progress
-# Filter by date, sender, recipient, and direction (in/out)  
-# Sort by date or amount  
-# Withdraw  
-
-
 transactions_router = APIRouter(prefix='/transactions')
 
-
-@transactions_router.get('/user_id/{sender_id}')  
-def get_transactions(sender_id: int):
-     '''Shows a list of all the user's transactions.\n
-     Parameters explanation to follow.
+@transactions_router.get('/')  
+def get_users_transactions(current_user: int = Depends(get_current_user)):
+     '''
+     This function returns a list of all the transactions for the specified user.
+     
+     Parameters:
+     current_user: int
+        The ID of the currently authenticated user, automatically injected by Depends(get_current_user).
+        This parameter is used to ensure that the request is made by an authenticated user.
      '''
 
-     transactions_lst = transactions_service.show_all_transactions(sender_id)
+     # if not current_user:
+     #      return BadRequest(content=f'You currently have no access to this section, please login.') 
+
+     transactions_lst = transactions_service.show_all_transactions(current_user)
 
      return transactions_lst 
 
-
-
 @transactions_router.get('/id/{transaction_id}') 
-def get_transactions_by_id():
-     '''Shows more detailed information about a user's transactions.\n
-     Parameters explanation to follow.
+def get_transactions_by_id(transaction_id: int, current_user: int = Depends(get_current_user)):
      '''
-     pass
+     This finction returns a more detailed information about a user's transactions.
+
+     Parameters:
+     transaction_id : int
+        The ID of the transaction to retrieve details for.
+
+     current_user : int
+        The ID of the currently authenticated user, automatically injected by Depends(get_current_user).
+        This parameter is used to ensure that the request is made by an authenticated user.
+     '''
+     
+     transaction = transactions_service.show_transaction_by_id(transaction_id)
+
+     if transaction is None:
+          return NotFound() # status_code=404
+     else:
+          return transaction
 
 @transactions_router.post('/wallet', status_code=201) 
 def add_money_to_wallet():
-     '''Makes a transaction to the wallet's ballance.\n
+     '''Makes a transaction to the user wallet's ballance.\n
      Parameters explanation to follow.
      '''
      pass
@@ -50,11 +61,11 @@ def make_a_transaction():
      '''
      pass
 
-# @transactions_router.put('/approval', status_code=201) # POST or PUT? 
-# def approve_a_transaction():
-#      '''Makes a transaction to another user or category.\n
-#      Parameters explanation to follow.
-#      '''
-#      pass
+@transactions_router.put('/approval', status_code=201)  
+def approve_a_transaction():
+     '''Confirmes or declines a transaction.\n
+     Parameters explanation to follow.
+     '''
+     pass
 
 
