b0992eee8092d10d4fa8b23ce88650b3424a4007
eahristovaa5 <eahristovaa@gmail.com>
2024-05-20 15:50:39 +0530

    transaction_service

--- services/transactions_service.py
+++ services/transactions_service.py
@@ -2,30 +2,55 @@ from data.models.transactions import Transaction
 from data.database_queries import read_query, insert_query, update_query
 from schemas import transactions
 
-# service which will get from the DB all of the transactions a list - 
-def show_all_transactions(sender_id: int):
-     '''Explanation to follow.\n
-     Parameters explanation to follow.
+def show_all_transactions(current_user: int):
+     '''
+     This function returns a list of all the transactions for the authenticated user.
+
+     Parameters:
+     current_user : int
+        The ID of the currently authenticated user, automatically injected by Depends(get_current_user).
+        This parameter is used to ensure that the request is made by an authenticated user.
+
      '''
      transactions = read_query('''SELECT id, status, transaction_date, amount, next_payment, categories_id, sender_id, receiver_id, cards_id
                                       FROM transactions
-                                      WHERE sender_id= ?''',
-                               (sender_id,))
+                                      WHERE sender_id = ?''',
+                               (current_user,))
      
      transactions_data = []
      for row in transactions:
           transaction = Transaction.from_query_result(*row)
           transaction.transaction_date = transaction.transaction_date.strftime('%Y/%m/%d %H:%M')
+          transaction.next_payment = transaction.next_payment.strftime('%Y/%m/%d %H:%M')
           transactions_data.append(transaction)
 
      return transactions_data
 
-# service which will get from the DB a single transactions and all of its attributes - 
-def show_transaction_by_id():
-     '''Explanation to follow.\n
-     Parameters explanation to follow.
+def show_transaction_by_id(transaction_id: int):
      '''
-     pass
+     This finction returns a more detailed information about a user's transactions.
+
+     Parameters:
+     transaction_id : int
+        The ID of the transaction to retrieve details for.
+
+     current_user : int
+        The ID of the currently authenticated user, automatically injected by Depends(get_current_user).
+        This parameter is used to ensure that the request is made by an authenticated user.
+     '''
+     transactions = read_query('''SELECT id, status, transaction_date, amount, next_payment, categories_id, sender_id, receiver_id, cards_id
+                                      FROM transactions
+                                      WHERE id = ?''',
+                               (transaction_id,))
+     
+     transaction = next((Transaction.from_query_result(*row) for row in transactions), None)
+
+     if transaction:
+          transaction.transaction_date = transaction.transaction_date.strftime('%Y/%m/%d %H:%M')
+          transaction.next_payment = transaction.next_payment.strftime('%Y/%m/%d %H:%M')
+     
+     return transaction
+
 
 # service which will add money to the balance attribute for the users column in the DB - 
 def add_money_to_users_ballnace():
