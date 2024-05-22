f3a813927c7c79b0247c56b0ca8897341506e708
eahristovaa5 <eahristovaa@gmail.com>
2024-05-22 13:21:15 +0530

    transactions_endpoints

--- routers/transactions.py
+++ routers/transactions.py
@@ -11,85 +11,92 @@ transactions_router = APIRouter(prefix='/transactions')
 
 @transactions_router.get('/', status_code=201, tags=['Transactions'])  
 def get_users_transactions(current_user: int = Depends(get_current_user)):
-     '''
-     This function returns a list of all the transactions for the specified user.\n
-     
-     Parameters:\n
-     - current_user: int\n
-        - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
-        - This parameter is used to ensure that the request is made by an authenticated user.
-     '''
+   '''
+   This function returns a list of all the transactions for the specified user.\n
 
-     # if not current_user:
-     #      return BadRequest(content=f'You currently have no access to this section, please login.') 
+   Parameters:\n
+   - current_user: int\n
+      - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
+      - This parameter is used to ensure that the request is made by an authenticated user.
+   ''' 
 
-     transactions_lst = transactions_service.show_all_transactions(current_user)
+   transactions_lst = transactions_service.show_all_transactions(current_user)
 
-     return transactions_lst 
+   return transactions_lst 
 
 @transactions_router.get('/id/{transaction_id}', status_code=201, tags=['Transactions']) 
 def get_transactions_by_id(transaction_id: int, current_user: int = Depends(get_current_user)):
-     '''
-     This finction returns a more detailed information about a user's transactions.\n
+   '''
+   This function returns a more detailed information about a user's transactions.\n
 
-     Parameters:\n
-     - transaction_id : int\n
-        - The ID of the transaction to retrieve details for.\n
-     - current_user : int\n
-        - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
-        - This parameter is used to ensure that the request is made by an authenticated user.
-     '''
-     
-     transaction = transactions_service.show_transaction_by_id(transaction_id)
+   Parameters:\n
+   - transaction_id : int\n
+      - The ID of the transaction to retrieve details for.\n
+   - current_user : int\n
+      - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
+      - This parameter is used to ensure that the request is made by an authenticated user.
+   '''
 
-     if transaction is None:
-          return NotFound() # status_code=404
-     else:
-          return transaction
+   transaction = transactions_service.show_transaction_by_id(transaction_id)
+
+   if transaction is None:
+      return NotFound() # status_code=404
+   else:
+      return transaction
 
 @transactions_router.post('/wallet', status_code=201, tags=['Transactions']) 
 def add_money_to_wallet(transaction: Transaction, current_user: int = Depends(get_current_user)):
-     '''This function makes a transaction to the user wallet's ballance.\n
+   '''This function makes a transaction to the user wallet's ballance.\n
 
-     Parameters:\n
-     - transaction : Transaction\n
-        - The transaction details to be added to the user's wallet.\n
-     - current_user: int\n
-        - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
-        - This parameter is used to ensure that the request is made by an authenticated user.
-     '''
-     
-     transaction.transaction_date = datetime.now()
-     transaction.transaction_date = transaction.transaction_date.strftime("%Y/%m/%d %H:%M")
+   Parameters:\n
+   - transaction : Transaction\n
+      - The transaction details to be added to the user's wallet.\n
+   - current_user: int\n
+      - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
+      - This parameter is used to ensure that the request is made by an authenticated user.
+   '''
 
-     return transactions_service.add_money_to_users_ballnace(transaction, current_user)
+   transaction.transaction_date = datetime.now()
+   transaction.transaction_date = transaction.transaction_date.strftime("%Y/%m/%d %H:%M")
+
+   return transactions_service.add_money_to_users_ballnace(transaction, current_user)
 
 @transactions_router.post('/', status_code=201, tags=['Transactions']) 
 def make_a_transaction(transaction: Transaction, current_user: int = Depends(get_current_user)):
-     '''
-     This function makes a transaction to another user or category.\n
+   '''
+   This function makes a transaction to another user or category.\n
 
-     Parameters:\n
-     - transaction : Transaction\n
-        - The transaction details to be added to the user's wallet.\n
-     - current_user: int\n
-        - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
-        - This parameter is used to ensure that the request is made by an authenticated user.
-     '''
-     
-     transaction.transaction_date = datetime.now()
-     transaction.transaction_date = transaction.transaction_date.strftime("%Y/%m/%d %H:%M")
+   Parameters:\n
+   - transaction : Transaction\n
+      - The transaction details to be added to the user's wallet.\n
+   - current_user: int\n
+      - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
+      - This parameter is used to ensure that the request is made by an authenticated user.
+   '''
 
-     transaction.receiver_id = user_services.user_id_exists(transaction.receiver_id)
-     transaction.categories_id = categories_service.find_category_by_id(transaction.categories_id)
+   transaction.transaction_date = datetime.now()
+   transaction.transaction_date = transaction.transaction_date.strftime("%Y/%m/%d %H:%M")
 
-     if transaction.receiver_id and transaction.categories_id:
-          return transactions_service.create_transactions(transaction)
+   transaction.receiver_id = user_services.user_id_exists(transaction.receiver_id)
+   transaction.categories_id = categories_service.find_category_by_id(transaction.categories_id)
 
-@transactions_router.put('/approval', status_code=201, tags=['Transactions'])  
-def approve_a_transaction():
-     '''
-     Confirmes or declines a transaction.\n
-     Parameters explanation to follow.
-     '''
-     pass
+   if transaction.receiver_id and transaction.categories_id:
+      return transactions_service.create_transactions(transaction)
+
+@transactions_router.put('/approval/id/{transaction_id}', status_code=201, tags=['Transactions'])  
+def approve_a_transaction(transaction_id: int, current_user: int = Depends(get_current_user)):
+   '''
+   This function confirmes or declines a transaction.\n
+
+   Parameters:\n
+   - transaction_id : int\n
+      - The ID of the transaction to retrieve details for.\n
+   - current_user : int\n
+      - The ID of the currently authenticated user, automatically injected by Depends(get_current_user).\n
+      - This parameter is used to ensure that the request is made by an authenticated user.
+   '''
+   if not transactions_service.transaction_id_exists(transaction_id):
+      return BadRequest(content=f'Transaction {transaction_id} does not exist.')
+
+   transaction = transactions_service.approve_transaction(transaction_id)
+   return transaction
