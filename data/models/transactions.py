61233bb3e1b4045f9ccc0f39d1eaf3175ca4b8ee
eahristovaa5 <eahristovaa@gmail.com>
2024-05-22 23:16:43 +0530

    transactions_models_changes

--- data/models/transactions.py
+++ data/models/transactions.py
@@ -7,20 +7,18 @@ class Transaction(BaseModel):
     status: str = 'pending'
     transaction_date: datetime 
     amount: float
-    categories_id: int | None = None
     sender_id: int | None = None
     receiver_id: int
     cards_id: int | None = None
 
     @classmethod
     def from_query_result(cls, id, status, transaction_date, amount, 
-                          categories_id, sender_id, receiver_id, cards_id):
+                          sender_id, receiver_id, cards_id):
         return cls(
             id=id,
             status=status,
             transaction_date=transaction_date,
             amount=amount,
-            categories_id=categories_id,
             sender_id=sender_id,
             receiver_id=receiver_id,
             cards_id=cards_id
