f3bbf14f302295303efada0f49ca2cb1e5f01fd3
Kristiyand97 <kristiyand97@gmail.com>
2024-05-24 19:11:08 +0300

    - structure update

--- routers/admin.py
+++ routers/admin.py
@@ -1,22 +1,13 @@
 from fastapi import APIRouter, status, HTTPException, Depends
-import security.password_hashing
-import services.user_services
 from common import authorization
-from common.authorization import create_access_token
-from common.helper_functions import check_password
-from common.wallet_info import detailed_info
-from schemas.contact import ContactCreate
-from services.user_services import view
-from schemas.user import UserCreate, UserOut, UserLogin
-from security.password_hashing import verify_password
-from services import user_services
-from services.user_services import view_profile
+from schemas.transactions import TransactionFilters
 from services import admin_services
+from services.user_services import view_user_transactions, pending_transactions
 
 admin_router = APIRouter(prefix='/admin')
 
 
-@admin_router.get("/all_users")
+@admin_router.get("/all_users", tags=["Admin"])
 def get_all_users(search: str = None, page: int = 1, size: int = 10,
                   current_user: int = Depends(authorization.get_current_user)):
     if admin_services.check_if_not_admin(current_user):
@@ -28,7 +19,7 @@ def get_all_users(search: str = None, page: int = 1, size: int = 10,
     admin_services.get_all_users(search, page, size)
 
 
-@admin_router.put("/block/{user_id}")
+@admin_router.put("/block/{user_id}", tags=["Admin"])
 def block_user(user_id: int, current_user: int = Depends(authorization.get_current_user)):
     if admin_services.check_if_not_admin(current_user):
         raise HTTPException(
@@ -39,7 +30,7 @@ def block_user(user_id: int, current_user: int = Depends(authorization.get_curre
     return admin_services.block_user(user_id)
 
 
-@admin_router.put("/unblock/{user_id}")
+@admin_router.put("/unblock/{user_id}", tags=["Admin"])
 def unblock_user(user_id: int, current_user: int = Depends(authorization.get_current_user)):
     if admin_services.check_if_not_admin(current_user):
         raise HTTPException(
@@ -50,7 +41,7 @@ def unblock_user(user_id: int, current_user: int = Depends(authorization.get_cur
     return admin_services.unblock_user(user_id)
 
 
-@admin_router.put("/approve/{email}")
+@admin_router.put("/approve/{email}", tags=["Admin"])
 def approve_user(email: str, current_user: int = Depends(authorization.get_current_user)):
     if admin_services.check_if_not_admin(current_user):
         raise HTTPException(
@@ -59,3 +50,32 @@ def approve_user(email: str, current_user: int = Depends(authorization.get_curre
         )
 
     return admin_services.approve_user(email)
+
+
+@admin_router.get('/admin/{user_id}', tags=["Admin"])
+def get_user_transactions_(
+        user_id: int,
+        current_user: int = Depends(authorization.get_current_user),
+        filters: TransactionFilters = Depends()
+):
+    result = view_user_transactions(user_id, current_user, filters)
+
+    if isinstance(result, str):
+        raise HTTPException(status_code=403, detail=result)
+
+    return result
+
+
+@admin_router.post('/admin/{user_id}', tags=["Admin"])
+def deny_user_pending_transactions(
+        user_id: int,
+        current_user: int = Depends(authorization.get_current_user)
+):
+    result = pending_transactions(current_user, user_id)
+
+    if isinstance(result, str) and result.startswith("Not authorized"):
+        raise HTTPException(status_code=403, detail=result)
+    elif isinstance(result, str):
+        raise HTTPException(status_code=400, detail=result)
+
+    return {"message": result}
