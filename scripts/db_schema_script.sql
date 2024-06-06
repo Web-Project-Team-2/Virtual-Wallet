061b97ed15821df0c40bdcdb6dbe1ef599f89f2b
eahristovaa5 <eahristovaa@gmail.com>
2024-06-06 18:35:33 +0300

    db_schema_script

--- scripts/db_schema_script.sql
+++ scripts/db_schema_script.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema e_wallet
 -- -----------------------------------------------------
 
@@ -12,9 +15,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema e_wallet
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `e_wallet` DEFAULT CHARACTER SET latin1 ;
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 USE `e_wallet` ;
 
 -- -----------------------------------------------------
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `e_wallet`.`users` (
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
   UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 117
+AUTO_INCREMENT = 13
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -51,18 +51,17 @@ CREATE TABLE IF NOT EXISTS `e_wallet`.`cards` (
   `expiration_date` DATE NOT NULL,
   `card_status` ENUM('active', 'not active') NULL DEFAULT 'active',
   `user_id` INT(11) NOT NULL,
-  `balance` FLOAT NULL DEFAULT 0,
+  `balance` FLOAT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `card_number_UNIQUE` (`card_number` ASC) VISIBLE,
-  INDEX `fk_cards_users1_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cards_users1`
+  INDEX `fk_cards_users_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_cards_users`
     FOREIGN KEY (`user_id`)
     REFERENCES `e_wallet`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 21
+AUTO_INCREMENT = 14
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -73,8 +72,7 @@ CREATE TABLE IF NOT EXISTS `e_wallet`.`categories` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = latin1;
@@ -86,14 +84,14 @@ DEFAULT CHARACTER SET = latin1;
 CREATE TABLE IF NOT EXISTS `e_wallet`.`contacts` (
   `users_id` INT(11) NOT NULL,
   `contact_user_id` INT(11) NOT NULL,
-  INDEX `fk_contacts_users_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_contacts_users1_idx` (`contact_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_contacts_users`
+  INDEX `fk_contacts_users1_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_contacts_users2_idx` (`contact_user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_contacts_users1`
     FOREIGN KEY (`users_id`)
     REFERENCES `e_wallet`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_contacts_users1`
+  CONSTRAINT `fk_contacts_users2`
     FOREIGN KEY (`contact_user_id`)
     REFERENCES `e_wallet`.`users` (`id`)
     ON DELETE NO ACTION
@@ -107,23 +105,35 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `e_wallet`.`recurring_transactions` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `recurring_transaction_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `next_payment` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-  `users_id` INT(11) NOT NULL,
+  `status` ENUM('pending', 'confirmed', 'declined') NULL DEFAULT 'pending',
+  `condition` ENUM('edited', 'sent', 'cancelled') NULL DEFAULT 'edited',
+  `amount` FLOAT NOT NULL,
+  `sender_id` INT(11) NOT NULL,
+  `receiver_id` INT(11) NOT NULL,
   `categories_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_recurring_transactions_users1_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_recurring_transactions_users1_idx` (`sender_id` ASC) VISIBLE,
   INDEX `fk_recurring_transactions_categories1_idx` (`categories_id` ASC) VISIBLE,
+  INDEX `fk_recurring_transactions_users2_idx` (`receiver_id` ASC) VISIBLE,
   CONSTRAINT `fk_recurring_transactions_categories1`
     FOREIGN KEY (`categories_id`)
     REFERENCES `e_wallet`.`categories` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_recurring_transactions_users1`
-    FOREIGN KEY (`users_id`)
+    FOREIGN KEY (`sender_id`)
+    REFERENCES `e_wallet`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recurring_transactions_users2`
+    FOREIGN KEY (`receiver_id`)
     REFERENCES `e_wallet`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
+AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -133,8 +143,10 @@ DEFAULT CHARACTER SET = latin1;
 CREATE TABLE IF NOT EXISTS `e_wallet`.`transactions` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `status` ENUM('pending', 'confirmed', 'declined') NOT NULL DEFAULT 'pending',
+  `condition` ENUM('edited', 'sent', 'cancelled') NOT NULL DEFAULT 'edited',
   `transaction_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `amount` FLOAT NOT NULL,
+  `category_name` VARCHAR(45) NOT NULL DEFAULT 'no category',
   `sender_id` INT(11) NOT NULL,
   `receiver_id` INT(11) NOT NULL,
   `cards_id` INT(11) NOT NULL,
@@ -159,7 +171,7 @@ CREATE TABLE IF NOT EXISTS `e_wallet`.`transactions` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 14
+AUTO_INCREMENT = 31
 DEFAULT CHARACTER SET = latin1;
 
 
